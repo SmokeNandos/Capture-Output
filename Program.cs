ba45a501e2c446a5cb38bf7b2a2b386b58192e20
SmokeNandos <67119216+SmokeNandos@users.noreply.github.com>
2020-06-20 01:52:29 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capture_Output
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string designer = "Anders Hejlsberg";
+
+            int indexOfSpace = designer.IndexOf(" ");
+            string secondName = designer.Substring(indexOfSpace);
+
+            Console.WriteLine(secondName);
+            Console.ReadLine();
+        }
+    }
+}
